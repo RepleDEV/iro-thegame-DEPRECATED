df36994a899c6cdb66b1dd55dc310932d6e8ee4d
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-04-24 20:13:39 +0700

    Release v1.1.0

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const startMenu = document.getElementById('startmenu');
 const mainMenu = document.getElementById('mainMenu');
 const subMenu = document.getElementById('subMenu');
 const diffBtn = document.getElementById('difficulty_btn');
+const startInfo = document.getElementById('clickInfo');
+const sliderDiv = document.getElementById('sliders');   
+const logoDiv = document.getElementById('imgStart');
 
 const slider_r = document.getElementById('slider_red');
 const slider_g = document.getElementById('slider_green');
@@ -16,9 +19,12 @@ const inputValue_b = document.getElementById('input_B')
 const helpBtn = document.getElementById('helpBtn');
 const helpTextDiv = document.getElementById('helpText');
 
-const outputText = document.getElementById('result-output');
+const statusColor = document.getElementById("statusColor");
 
 var color_bitrate = 0;
+var step = 0;
+
+var colorDistance = 0;
 
 function round(n , r, ceil) {
     var acceptedNumbers = []
@@ -61,25 +67,45 @@ function easyDiff(){
     startGame();
 
     color_bitrate = 4
+    step = 256 / Math.pow(2, color_bitrate);
 
-    slider_r.step = 256 / Math.pow(2, color_bitrate)
-    // console.log(slider_r.step);
+    slider_r.step = step - 1;
+    slider_g.step = step - 1;
+    slider_b.step = step - 1;
+
+    inputValue_r.step = step - 1;
+    inputValue_g.step = step - 1;
+    inputValue_b.step = step - 1;
 }
 function medDiff(){
     startGame();
 
     color_bitrate = 6;
 
-    slider_r.step = 256 / Math.pow(2, color_bitrate)
-    // console.log(slider_r.step);
+    step = 256 / Math.pow(2, color_bitrate);
+
+    slider_r.step = step - 1
+    slider_g.step = step - 1
+    slider_b.step = step - 1
+
+    inputValue_r.step = step - 1;
+    inputValue_g.step = step - 1;
+    inputValue_b.step = step - 1;
 }
 function hardDiff() {
     startGame();
 
     color_bitrate = 8;
 
-    slider_r.step = 256 / Math.pow(2, color_bitrate)
-    // console.log(slider_r.step);
+    step = 256 / Math.pow(2, color_bitrate);
+
+    slider_r.step = step - 1
+    slider_g.step = step - 1
+    slider_b.step = step - 1
+
+    inputValue_r.step = step - 1;
+    inputValue_g.step = step - 1;
+    inputValue_b.step = step - 1;
 }
 
 
@@ -91,119 +117,119 @@ function randomColor() {
     return [rng_r, rng_g, rng_b];
 }
 
-statusOutputs = ['v','v','v']
 
-function updateStatus() {
-    for (var i = 0; i < statusOutput.length; i++){
-        if (statusOutput[i] == 0){
-            statusOutputs[i] = '<'
-            outputText.innerHTML = 'Status:R' + statusOutputs[0] + ';G' + statusOutputs[1] + ';B' + statusOutputs[2];
-            return;
+function updateStatus(v1,v2,v3) {
+    var points = [0,0,0];
+    var colors = [parseInt(bg_color_r),parseInt(bg_color_g),parseInt(bg_color_b)];
+    var i;
+    for (i = 0;i<colors.length;i++) {
+        if (colors[i] < randomizedColor[i])points[i] = (colors[i] * (256/randomizedColor[i])) / 256;
+        else if (colors[i] > randomizedColor[i])points[i] = (colors[i] / (256*randomizedColor[i])) * 256;
+        if (colors[i] == randomizedColor[i])points[i] = 1;
+    }
+    var format = `rgb(`;
+    for (i = 0;i<points.length;i++){
+        if (points[i] < 1){
+            format += `${points[i] * 256},`;
         }
-        else if (statusOutput[i] == 1){
-            statusOutputs[i] = '>'
-            outputText.innerHTML = 'Status:R' + statusOutputs[0] + ';G' + statusOutputs[1] + ';B' + statusOutputs[2] ;
-            return;
+        else if (points[i] > 1 && points[i] < 2){
+            format += `${(2 - points[i]) * 256},`;
+        }
+        else if (points[i] > 2){
+            format += `0,`;
+        }
+        else if (points[i] == 1) {
+            format += `255,`;
+        } else if (points[i] == 0){
+            format += `0,`;
         }
     }
-    if (statusOutput == randomizedColor){
-        outputText.innerHTML = 'Congratulations! You won! Thank you for <i>beta</i> testing!'
+    format = format.substring(0,format.length-1);
+    statusColor.style.backgroundColor = `${format})`
+
+    var score = points[0] + points[1] + points[2];
+    if (score == 3){
+        changeDisabilityStatus(true);
+        statusColor.style.backgroundColor = '';
+        statusColor.style.background = '';
+        statusColor.style.width = '100%';
+        statusColor.innerHTML = `<p>Congratulations! You won!<br>If you have any feature suggestions/bug reports please open an issue in <a href='https://github.com/RepleDEV/iro-thegame' style='color:rgb(${randomizedColor[0]},${randomizedColor[1]},${randomizedColor[2]});'>this GitHub repo</a>.<br>Thank you for playing!`;
+        return;
     } 
+    return;
 }
+function updateBackground(r,g,b) {
+    r = r.toString();
+    g = g.toString();
+    b = b.toString();
+    body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+function updateInput(element, value) {element.value = parseInt(value);}
+function updateSlider(slider, value) {slider.value = parseInt(value);}
+
 var randomizedColor = [0,0,0];
 
-var bg_color_r = "0";
-var bg_color_g = "0";
-var bg_color_b = "0";
+var bg_color_r = 0;
+var bg_color_g = 0;
+var bg_color_b = 0;
 
-var statusOutput = [0,0,0];
+var clickStatus = false;
 
 slider_r.oninput = () => {
     bg_color_r = slider_r.value
-    body.style.backgroundColor = 'rgb(' + bg_color_r + ',' + bg_color_g + ',' + bg_color_b + ')';
-    inputValue_r.value = parseInt(bg_color_r);
-    if (slider_r.value == randomizedColor[0]){
-        console.log("correct!")
-    }
-    if (slider_r.value < randomizedColor[0]){
-        statusOutput[0] = 0;
-    } else if (slider_r.value > randomizedColor[0]){
-        statusOutput[0] = 1;
-    }
-    updateStatus();
+    updateBackground(bg_color_r,bg_color_g,bg_color_b);
+    updateInput(inputValue_r,bg_color_r);
+    updateStatus(bg_color_r,bg_color_g,bg_color_b);
 }
 slider_g.oninput = () => {
-    bg_color_g = slider_g.value
-    body.style.backgroundColor = 'rgb(' + bg_color_r + ',' + bg_color_g + ',' + bg_color_b + ')';
-    inputValue_g.value = parseInt(bg_color_g);
-    if (slider_g.value == randomizedColor[1]){
-        console.log("correct!")
-    }
-    if (slider_g.value < randomizedColor[1]){
-        statusOutput[1] = 0
-    } else if (slider_g.value > randomizedColor[1]){
-        statusOutput[1] = 1;
-    }
-    updateStatus();
+    bg_color_g = slider_g.value;
+    updateBackground(bg_color_r,bg_color_g,bg_color_b);
+    updateInput(inputValue_g,bg_color_g);
+    updateStatus(bg_color_r,bg_color_g,bg_color_b);
 }
 slider_b.oninput = () => {
-    bg_color_b = slider_b.value
-    body.style.backgroundColor = 'rgb(' + bg_color_r.toString() + ',' + bg_color_g.toString() + ',' + bg_color_b.toString() + ')';
-    inputValue_b.value = parseInt(bg_color_b);
-    if (slider_b.value == randomizedColor[2]){
-        console.log("correct!")
-    }
-    if (slider_b.value < randomizedColor[2]){
-        statusOutput[2] = 0
-    } else if (slider_b.value > randomizedColor[2]){
-        statusOutput[2] = 1;
-    }
-    updateStatus();
+    bg_color_b = slider_b.value;
+    updateBackground(bg_color_r,bg_color_g,bg_color_b);
+    updateInput(inputValue_b,bg_color_b);
+    updateStatus(bg_color_r,bg_color_g,bg_color_b);
 }
 inputValue_r.oninput = () => {
+    bg_color_r = inputValue_r.value;
+    updateSlider(slider_r,bg_color_r);
+    updateBackground(bg_color_r,bg_color_g,bg_color_b);
     if (inputValue_r.value !== "") {bg_color_r = inputValue_r.value.toString();}
-    else {bg_color_r = ""}
-    slider_r.value = bg_color_r;
-    body.style.backgroundColor = 'rgb(' + bg_color_r.toString() + ',' + bg_color_g.toString() + ',' + bg_color_b.toString() + ')';
-    if (slider_b.value == randomizedColor[0]){
-    }
-    if (inputValue_r < randomizedColor[0]) {statusOutput[0] = 0}
-    else if (inputValue_r > randomizedColor[0]) {statusOutput[0] = 1}
-    updateStatus();
+    else {bg_color_r = 0}
+    updateStatus(bg_color_r,bg_color_g,bg_color_b);
 }
 inputValue_g.oninput = () => {
+    bg_color_g= inputValue_g.value;
+    updateSlider(slider_g,bg_color_g);
+    updateBackground(bg_color_r,bg_color_g,bg_color_b);
     if (inputValue_g.value !== "") {bg_color_g = inputValue_g.value.toString();}
-    else {bg_color_g = ""}
-    slider_g.value = bg_color_g;
-    body.style.backgroundColor = 'rgb(' + bg_color_r.toString() + ',' + bg_color_g.toString() + ',' + bg_color_b.toString() + ')';
-    if (slider_g.value == randomizedColor[1]){
-        console.log("correct!")
-    }
-    if (inputValue_g < randomizedColor[1]) {statusOutput[1] = 0}
-    else if (inputValue_g > randomizedColor[1]) {statusOutput[1] = 1}
-    updateStatus();
+    else {bg_color_g = 0}
+    updateStatus(bg_color_r,bg_color_g,bg_color_b);
 }
 inputValue_b.oninput = () => {
+    bg_color_b = inputValue_b.value;
+    updateSlider(slider_b,bg_color_b);
+    updateBackground(bg_color_r,bg_color_g,bg_color_b);
     if (inputValue_b.value !== "") {bg_color_b = inputValue_b.value.toString();}
-    else {bg_color_b = ""}
-    slider_b.value = bg_color_b;
-    body.style.backgroundColor = 'rgb(' + bg_color_r.toString() + ',' + bg_color_g.toString() + ',' + bg_color_b.toString() + ')';
-    if (slider_b.value == randomizedColor[2]){
-        console.log("correct!")
-    }
-    if (slider_b.value < randomizedColor[2]) {
-        outputText.innerHTML = 'Status: R' + '<'
-    }
-    if (inputValue_b < randomizedColor[2]) {statusOutput[2] = 0}
-    else if (inputValue_b > randomizedColor[2]) {statusOutput[2] = 1}
-    updateStatus();
+    else {bg_color_b = 0}
+    updateStatus(bg_color_r,bg_color_g,bg_color_b);
 }
 startbtn.onclick = () => {
     randomizedColor = randomColor();
-    document.getElementById('colorPreview').style.backgroundColor = 'rgb(' + randomizedColor[0] + ',' + randomizedColor[1] + ',' + randomizedColor[2] + ')'
+    for (var i = 0; i < randomizedColor.length; i++) {
+        if (step == 1) step++;
+        randomizedColor[i] = round(randomizedColor[i], step - 1, 256);
+    }
+    document.getElementById('colorPreview').style.backgroundColor = 'rgb(' + randomizedColor[0] + ',' + randomizedColor[1] + ',' + randomizedColor[2] + ')';
+    changeDisabilityStatus(false);
+    console.log(randomizedColor);
 }
 
 const hideStartMenu = () => {
+    if (clickStatus == false)return;
     startMenu.style.opacity = 0;
     startMenu.setAttribute('class', 'hidden');
 
@@ -212,10 +238,8 @@ const hideStartMenu = () => {
 }
 
 const displayStartMenu = () => {
-    const title = document.getElementById('title');
-    const startInfo = document.getElementById('clickInfo');
-
-    var delay = 1200
+    setTimeout(() => window.scrollTo(0,0), 10)
+    var delay = 1200;
 
     changeDisabilityStatus(true);
 
@@ -224,12 +248,17 @@ const displayStartMenu = () => {
     startMenu.setAttribute('class', 'startMenu');
     helpTextDiv.style.opacity = 0;
     document.getElementById('hrStart').style.width = '24rem';
-    document.getElementById('sliders').style.opacity = 0;
+    sliderDiv.style.display = 'none';
+    sliderDiv.style.opacity = 0;
     setTimeout(() => {
-        startMenu.style.opacity = 1;
+        logoDiv.style.opacity = 1;
     }, delay)
     setTimeout(() => {
-        startInfo.style.opacity = 1;
+        logoDiv.style.opacity = 0;
+        clickStatus = true;
+        setTimeout(() => logoDiv.style.display = 'none', 400);
+        setTimeout(() => startInfo.style.opacity = 1,800);
+        setTimeout(() => startMenu.style.opacity = 1,1400);
     }, delay + 1800)
 }
 
@@ -243,7 +272,6 @@ helpBtn.onclick = () => {
 const showDrop = () => {document.getElementById('diffMenu').classList.toggle('show')};
 
 window.onclick = (event) => {
-    console.log('e')
     if (!event.target.matches('.dropBtn')){
         var dropDowns = document.getElementsByClassName('diffMenu');
         for (let i = 0; i < dropDowns.length; i++){
@@ -254,15 +282,32 @@ window.onclick = (event) => {
 }
 
 function changeDisabilityStatus(status) {
-    slider_r.disabled = status; slider_g.disabled = status; slider_b.disabled = status;
+    slider_r.disabled = status;slider_g.disabled = status;slider_b.disabled = status;
     inputValue_b.disabled = status;inputValue_r.disabled = status;inputValue_g.disabled = status;
+    if (status == true) {
+        slider_r.setAttribute('class', 'sliders disabledSlider');
+        slider_g.setAttribute('class', 'sliders disabledSlider');
+        slider_b.setAttribute('class', 'sliders disabledSlider');
+        
+        inputValue_r.setAttribute('class', 'colorValues disabledInput');
+        inputValue_g.setAttribute('class', 'colorValues disabledInput');
+        inputValue_b.setAttribute('class', 'colorValues disabledInput');
+    } else if (status == false) {
+        slider_r.setAttribute('class', 'sliders sliderRed');
+        slider_g.setAttribute('class', 'sliders sliderGreen');
+        slider_b.setAttribute('class', 'sliders sliderBlue');
+
+        inputValue_r.setAttribute('class', 'colorValues');
+        inputValue_g.setAttribute('class', 'colorValues');
+        inputValue_b.setAttribute('class', 'colorValues');
+    }
 }
 
 function startGame() {
     subMenu.style.opacity = 0;
     subMenu.setAttribute('class', 'hidden');
     subMenu.style.display = 'none';
-
+    sliderDiv.style.display = 'block';
     mainMenu.removeAttribute('class');
     mainMenu.setAttribute('style', 'transition-duration: 1200ms; transition-timing-function: ease-in;')
     mainMenu.style.opacity = 1;
@@ -271,6 +316,4 @@ function startGame() {
     setTimeout(() => {
         document.getElementById('hrMain').style.width = '9.2rem';
     }, 400)
-
-    changeDisabilityStatus(false);
 }
