94d94b2170e678c0eef4f28480a9c1f7d314e4b6
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-05-05 11:07:54 +0700

    Release 2.0.06

--- src/index.js
+++ src/index.js
@@ -1,43 +1,79 @@
+// Full Script for iro-thegame
+
+// Main containers
 const body = document.getElementById('body');
-const startbtn = document.getElementById('startgame');
 const startMenu = document.getElementById('startmenu');
 const mainMenu = document.getElementById('mainMenu');
 const subMenu = document.getElementById('subMenu');
-const diffBtn = document.getElementById('difficulty_btn');
-const startInfo = document.getElementById('clickInfo');
-const sliderDiv = document.getElementById('sliders');   
-const logoDiv = document.getElementById('imgStart');
 
-const slider_r = document.getElementById('slider_red');
-const slider_g = document.getElementById('slider_green');
-const slider_b = document.getElementById('slider_blue');
+const colorPreview = document.getElementById('colorPreview'); // Preview of randomized color
+const statusColor = document.getElementById("statusColor"); // Color indicator
 
-const inputValue_r = document.getElementById('input_R')
-const inputValue_g = document.getElementById('input_G')
-const inputValue_b = document.getElementById('input_B')
+const inputContainer = document.getElementById('colorInputs'); // Container for inputs
+const logoContainer = document.getElementById('imgStart'); // Container for logo
 
-const helpBtn = document.getElementById('helpBtn');
-const helpTextDiv = document.getElementById('helpText');
+// Main buttons
+const submitBtn = document.getElementById('submitBtn'); // Submit button 
+const startbtn = document.getElementById('startgame'); // Start game button
+const helpBtn = document.getElementById('helpBtn'); // Help button
+const diffBtn = document.getElementById('difficulty_btn'); // Button / Dropdown to select difficulty
 
-const statusColor = document.getElementById("statusColor");
+const startText = document.getElementById('clickInfo'); // "Click anywhere to start" text
+const helpTextContainer = document.getElementById('helpText'); // It is what it is
 
-var color_bitrate = 0;
-var step = 0;
+// Main Sliders
+const slider_r = document.getElementById('slider_red'); // Red
+const slider_g = document.getElementById('slider_green'); // Green
+const slider_b = document.getElementById('slider_blue'); // Blue
 
-var colorDistance = 0;
+// Manual Inputs
+const inputValue_r = document.getElementById('input_R'); // Red
+const inputValue_g = document.getElementById('input_G'); // Green
+const inputValue_b = document.getElementById('input_B'); // Blue
 
-function round(n , r, ceil) {
-    var acceptedNumbers = []
+// Extra difficulty checkboxes
+const extraDifficulties_border = document.getElementById('border_checkbox'); // Border
+const extraDifficulties_submitBtn = document.getElementById('submit_checkbox'); // Submit
+const extraDifficulties_indicator  = document.getElementById('indicator_checkbox'); // Indicator
 
-    var high = 0;
-    var low = 0;
+// Global Variables
+var color_bitrate = 0; // Color bitrate (4/6/8)
+var step = 0; // Colour step (256 / Math.pow(2, color_bitrate) - 1)
+var score = 0; // Score. 1 = 1 correct primary color. 2 = 2 correct primary colors. 3 = all primary colors are correct
 
-    let i;
+// Options
+var enableIndicator = true;
+var enablePicker = false;
+var enableBorder = false;
+var enableSubmit = false;
 
+var clickStatus = false; // If false, user cant continue to start menu
+
+// Color picker. Color picker by iro.js (iro.js.org)
+var colorPicker = new iro.ColorPicker('#colorPicker',{
+    width:140, // Width
+    color:'#f00', // Initial color
+    display:'block', // Display 
+    margin:'20px', // Margin
+    wheelLightness:false, // Hard to explain, just check out the iro.js website (iro.js.org)
+    color:'white' // Again-
+});
+
+// Rounding function. Rounds numbers
+function round(n , r, ceil) { // n = number, r = the number it needs to round? ceil = max n
+    var acceptedNumbers = []; // Accepted numbers
+
+    var high = 0; // High value
+    var low = 0; // Low value
+
+    let i; // Index for for loop
+
+    // Updates acceptedNumbers
     for (i = 0; i < (ceil / r); i++){
         acceptedNumbers.push(i*r);
     }
 
+    // Updates high and low
     for (i = 0;i < acceptedNumbers.length; i++){
         var num = acceptedNumbers[i];
 
@@ -53,16 +89,22 @@ function round(n , r, ceil) {
         }
     }
 
+    // difference between the high and low values
     var diff = high - low;
-
-
-    if (parseInt(n.toString().charAt(n.toString.length + 1)) < diff / 2) {
-        return low;
-    } else {
-        return high;
-    }
+    // returns if... yea it's hard to explain cuz it's a lot of math
+    return parseInt(n.toString().charAt(n.toString.length + 1)) < diff / 2 ? low : high;
 }
+// Checks if the user has selected extra options
+function checkExtraDifficulties(){
+    // The extra difficulties
+    var extraDifficulties = [extraDifficulties_border,extraDifficulties_indicator,extraDifficulties_submitBtn];
 
+    // Checks them
+    if (extraDifficulties[0].checked == true)enableBorder = true;
+    if (extraDifficulties[1].checked != true)enableIndicator = false;
+    if (extraDifficulties[2].checked == true)enableSubmit = true;
+}
+// Easy difficulty
 function easyDiff(){
     startGame();
 
@@ -76,7 +118,10 @@ function easyDiff(){
     inputValue_r.step = step - 1;
     inputValue_g.step = step - 1;
     inputValue_b.step = step - 1;
+
+    checkExtraDifficulties();
 }
+// Medium
 function medDiff(){
     startGame();
 
@@ -91,7 +136,10 @@ function medDiff(){
     inputValue_r.step = step - 1;
     inputValue_g.step = step - 1;
     inputValue_b.step = step - 1;
+
+    checkExtraDifficulties();
 }
+// Hard
 function hardDiff() {
     startGame();
 
@@ -106,9 +154,11 @@ function hardDiff() {
     inputValue_r.step = step - 1;
     inputValue_g.step = step - 1;
     inputValue_b.step = step - 1;
+
+    checkExtraDifficulties();
 }
 
-
+// Decides random color
 function randomColor() {
     const rng_r = Math.floor(Math.random() * 256);
     const rng_g = Math.floor(Math.random() * 256);
@@ -117,8 +167,9 @@ function randomColor() {
     return [rng_r, rng_g, rng_b];
 }
 
-
+// Updates status / indicator
 function updateStatus(v1,v2,v3) {
+    if (!enableIndicator)return;
     var points = [0,0,0];
     var colors = [parseInt(bg_color_r),parseInt(bg_color_g),parseInt(bg_color_b)];
     var i;
@@ -146,131 +197,198 @@ function updateStatus(v1,v2,v3) {
     }
     format = format.substring(0,format.length-1);
     statusColor.style.backgroundColor = `${format})`
-
-    var score = points[0] + points[1] + points[2];
-    if (score == 3){
-        changeDisabilityStatus(true);
-        statusColor.style.backgroundColor = '';
-        statusColor.style.background = '';
-        statusColor.style.width = '100%';
-        statusColor.innerHTML = `<p>Congratulations! You won!<br>If you have any feature suggestions/bug reports please open an issue in <a href='https://github.com/RepleDEV/iro-thegame' style='color:rgb(${randomizedColor[0]},${randomizedColor[1]},${randomizedColor[2]});'>this GitHub repo</a>.<br>Thank you for playing!`;
-        return;
-    } 
+    score = 0;
+    for (i=0;i<colors.length;i++) if (colors[i] == randomizedColor[i]) score++; else return
+    if (score == 3 && !enableSubmit) {
+        endGame();
+    }
     return;
 }
+
+// Endgame... Ends the game :D
+function endGame(){
+    changeDisabilityStatus(true);
+    statusColor.style.backgroundColor = '';
+    statusColor.style.background = '';
+    statusColor.style.width = '100%';
+    statusColor.innerHTML = `<p>Congratulations! You won!<br>If you have any feature suggestions/bug reports please open an issue in <a href='https://github.com/RepleDEV/iro-thegame' style='color:rgb(${randomizedColor[0]},${randomizedColor[1]},${randomizedColor[2]});'>this GitHub repo</a>.<br>Thank you for playing!`;
+    startbtn.disabled = true;
+    enablePicker = false;   
+}
+
+// Updates the page background
 function updateBackground(r,g,b) {
     r = r.toString();
     g = g.toString();
     b = b.toString();
     body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 }
-function updateInput(element, value) {element.value = parseInt(value);}
-function updateSlider(slider, value) {slider.value = parseInt(value);}
 
-var randomizedColor = [0,0,0];
+// Updates the border (if the user selects the "border" extradifficulties)
+function updateBorder(){
+    if (!enableBorder)return;
+    else colorPreview.style.borderColor = `rgb(${bg_color_r.toString()},${bg_color_g.toString()},${bg_color_b.toString()})`;
+}
 
-var bg_color_r = 0;
-var bg_color_g = 0;
-var bg_color_b = 0;
+function updateInput(element, value) {element.value = parseInt(value);} // Update input
+function updateSlider(slider, value) {slider.value = parseInt(value);} // Update slider
+function updatePicker(values){colorPicker.color.rgb = { r: parseInt(values[0]), g: parseInt(values[1]), b: parseInt(values[2])}} // Update color picker
 
-var clickStatus = false;
+var randomizedColor = [0,0,0]; // Randomized color global var
 
-slider_r.oninput = () => {
+// Color values
+var bg_color_r = 0; // Red
+var bg_color_g = 0; // Green
+var bg_color_b = 0; // Blue
+
+// If the submit button was clicked
+submitBtn.onclick = () => {
+    // And the score is 3
+    if(score == 3)endGame(); // Endgame... /shrug
+}
+
+// Slider functions
+slider_r.oninput = () => { // Red slider
     bg_color_r = slider_r.value
     updateBackground(bg_color_r,bg_color_g,bg_color_b);
     updateInput(inputValue_r,bg_color_r);
     updateStatus(bg_color_r,bg_color_g,bg_color_b);
+    updateBorder();
+    updatePicker([bg_color_r,bg_color_g,bg_color_b]);
 }
-slider_g.oninput = () => {
+slider_g.oninput = () => { // Green slider
     bg_color_g = slider_g.value;
     updateBackground(bg_color_r,bg_color_g,bg_color_b);
     updateInput(inputValue_g,bg_color_g);
     updateStatus(bg_color_r,bg_color_g,bg_color_b);
+    updateBorder();
+    updatePicker([bg_color_r,bg_color_g,bg_color_b]);
 }
-slider_b.oninput = () => {
+slider_b.oninput = () => { // Blue slider
     bg_color_b = slider_b.value;
     updateBackground(bg_color_r,bg_color_g,bg_color_b);
     updateInput(inputValue_b,bg_color_b);
     updateStatus(bg_color_r,bg_color_g,bg_color_b);
+    updateBorder();
+    updatePicker([bg_color_r,bg_color_g,bg_color_b]);
 }
-inputValue_r.oninput = () => {
+
+// Input functions
+inputValue_r.oninput = () => { // Red input
     bg_color_r = inputValue_r.value;
     updateSlider(slider_r,bg_color_r);
     updateBackground(bg_color_r,bg_color_g,bg_color_b);
-    if (inputValue_r.value !== "") {bg_color_r = inputValue_r.value.toString();}
-    else {bg_color_r = 0}
+    bg_color_r = inputValue_r.value !== "" ? inputValue_r.value.toString() : 0;
     updateStatus(bg_color_r,bg_color_g,bg_color_b);
+    updateBorder();
+    updatePicker([bg_color_r,bg_color_g,bg_color_b]);
 }
-inputValue_g.oninput = () => {
+inputValue_g.oninput = () => { // Green input
     bg_color_g= inputValue_g.value;
     updateSlider(slider_g,bg_color_g);
     updateBackground(bg_color_r,bg_color_g,bg_color_b);
-    if (inputValue_g.value !== "") {bg_color_g = inputValue_g.value.toString();}
-    else {bg_color_g = 0}
+    bg_color_g = inputValue_g.value !== "" ? inputValue_g.value.toString() : 0;
     updateStatus(bg_color_r,bg_color_g,bg_color_b);
+    updateBorder();
+    updatePicker([bg_color_r,bg_color_g,bg_color_b]);
 }
-inputValue_b.oninput = () => {
+inputValue_b.oninput = () => { // Blue
     bg_color_b = inputValue_b.value;
     updateSlider(slider_b,bg_color_b);
     updateBackground(bg_color_r,bg_color_g,bg_color_b);
-    if (inputValue_b.value !== "") {bg_color_b = inputValue_b.value.toString();}
-    else {bg_color_b = 0}
+    bg_color_b = inputValue_b.value !== "" ? inputValue_b.value.toString() : 0;
     updateStatus(bg_color_r,bg_color_g,bg_color_b);
+    updateBorder();
+    updatePicker([bg_color_r,bg_color_g,bg_color_b]);
 }
+
+// If the start button is clilcked
 startbtn.onclick = () => {
-    randomizedColor = randomColor();
+    startbtn.innerHTML = "Reset Color"; // Change the text
+    randomizedColor = randomColor(); // Update randomizedColor
     for (var i = 0; i < randomizedColor.length; i++) {
         if (step == 1) step++;
         randomizedColor[i] = round(randomizedColor[i], step - 1, 256);
     }
-    document.getElementById('colorPreview').style.backgroundColor = 'rgb(' + randomizedColor[0] + ',' + randomizedColor[1] + ',' + randomizedColor[2] + ')';
-    changeDisabilityStatus(false);
-    console.log(randomizedColor);
+    colorPreview.style.backgroundColor = `rgb(${randomizedColor[0]},${randomizedColor[1]},${randomizedColor[2]})`;
+    changeDisabilityStatus(false); // enable sliders and inputs
+    enablePicker = true; // Enable color picker
+    colorPicker.on('color:change', (color) => { // Input listener for color picker
+        if (!enablePicker)return; // If enablePicker if false, return;
+        var {r,g,b} = color.rgb;
+        bg_color_r = round(r, step - 1, 256);
+        bg_color_g = round(g, step - 1, 256);
+        bg_color_b = round(b, step - 1, 256);
+        updateBackground(bg_color_r,bg_color_g,bg_color_b);
+        updateStatus(bg_color_r,bg_color_g,bg_color_b);
+        updateInput(inputValue_r,bg_color_r)
+        updateInput(inputValue_g,bg_color_g);
+        updateInput(inputValue_b,bg_color_b);
+        updateSlider(slider_r,bg_color_r);
+        updateSlider(slider_g,bg_color_g);
+        updateSlider(slider_b,bg_color_b);
+        updateBorder();
+        color.rgb = { r: bg_color_r, g:bg_color_g, b:bg_color_b};
+    });
+    if (!enableSubmit)submitBtn.disabled = true; // If enableSubmit is false, disable the button
+    else submitBtn.setAttribute('class', ''); // else, remove the "disabled" style
 }
 
-const hideStartMenu = () => {
-    if (clickStatus == false)return;
+// Function for hiding start menu
+function hideStartMenu() {
+    if (!clickStatus)return;
     startMenu.style.opacity = 0;
     startMenu.setAttribute('class', 'hidden');
 
-    subMenu.style.display = 'block';
+    subMenu.style.display = 'inline-block';
     setTimeout(() => subMenu.style.opacity = 1, 10)
+    extraDifficulties_indicator.checked = true;
 }
 
-const displayStartMenu = () => {
-    setTimeout(() => window.scrollTo(0,0), 10)
-    var delay = 1200;
+function displayStartMenu() {
+    setTimeout(() => window.scrollTo(0,0), 10); // So, there's this scrolling issue where when the page loads, it immediately scrolls to the bottom, and this was the only solution that i could think about.
 
-    changeDisabilityStatus(true);
+    const delay = 1200; // Animation delay
 
+    changeDisabilityStatus(true); // Disable sliders and inputs
+
+    // Animation stuff
+    mainMenu.style.display = 'none';
     subMenu.style.opacity = 0;
-
     startMenu.setAttribute('class', 'startMenu');
-    helpTextDiv.style.opacity = 0;
+    helpTextContainer.style.opacity = 0;
     document.getElementById('hrStart').style.width = '24rem';
-    sliderDiv.style.display = 'none';
-    sliderDiv.style.opacity = 0;
+    inputContainer.style.display = 'none';
+    inputContainer.style.opacity = 0;
+
     setTimeout(() => {
-        logoDiv.style.opacity = 1;
-    }, delay)
+        logoContainer.style.opacity = 1;
+    }, delay);
+
     setTimeout(() => {
-        logoDiv.style.opacity = 0;
+        logoContainer.style.opacity = 0;
         clickStatus = true;
-        setTimeout(() => logoDiv.style.display = 'none', 400);
-        setTimeout(() => startInfo.style.opacity = 1,800);
+        setTimeout(() => logoContainer.style.display = 'none', 400);
+        setTimeout(() => startText.style.opacity = 1,800);
         setTimeout(() => startMenu.style.opacity = 1,1400);
-    }, delay + 1800)
+    }, delay + 1800);
 }
 
+// If the help button was clicked
 helpBtn.onclick = () => {
-    helpTextDiv.style.display = 'block';
+    helpTextContainer.style.display = 'block';
     setTimeout(() => {
-        helpTextDiv.style.opacity = 1;
-    }, 1)
+        helpTextContainer.style.opacity = 1;
+    }, 1);
+    setTimeout(() => window.scrollTo(0,0), 15000);
 }
 
-const showDrop = () => {document.getElementById('diffMenu').classList.toggle('show')};
+// Dropdown js, html, and css, by w3schools.com and modified by me
 
+// Show difficulty dropdown function
+function showDrop() {document.getElementById('diffMenu').classList.toggle('show')};
+
+// Closes the dropdown if the user clicks out of the dropdown
 window.onclick = (event) => {
     if (!event.target.matches('.dropBtn')){
         var dropDowns = document.getElementsByClassName('diffMenu');
@@ -281,10 +399,12 @@ window.onclick = (event) => {
     }
 }
 
-function changeDisabilityStatus(status) {
+// Change slider and input disability status
+function changeDisabilityStatus(status) { // If status = false, enable the sliders and inputs. Else if status = true, disable the sliders and inputs
     slider_r.disabled = status;slider_g.disabled = status;slider_b.disabled = status;
     inputValue_b.disabled = status;inputValue_r.disabled = status;inputValue_g.disabled = status;
-    if (status == true) {
+
+    if (status) {
         slider_r.setAttribute('class', 'sliders disabledSlider');
         slider_g.setAttribute('class', 'sliders disabledSlider');
         slider_b.setAttribute('class', 'sliders disabledSlider');
@@ -292,7 +412,7 @@ function changeDisabilityStatus(status) {
         inputValue_r.setAttribute('class', 'colorValues disabledInput');
         inputValue_g.setAttribute('class', 'colorValues disabledInput');
         inputValue_b.setAttribute('class', 'colorValues disabledInput');
-    } else if (status == false) {
+    } else if (!status) {
         slider_r.setAttribute('class', 'sliders sliderRed');
         slider_g.setAttribute('class', 'sliders sliderGreen');
         slider_b.setAttribute('class', 'sliders sliderBlue');
@@ -303,17 +423,18 @@ function changeDisabilityStatus(status) {
     }
 }
 
+// Start the game. Again, animations. (i should've just used anime.js lol)
 function startGame() {
     subMenu.style.opacity = 0;
     subMenu.setAttribute('class', 'hidden');
     subMenu.style.display = 'none';
-    sliderDiv.style.display = 'block';
+    inputContainer.style.display = 'block';
+    mainMenu.style.display = 'inline-block'
     mainMenu.removeAttribute('class');
     mainMenu.setAttribute('style', 'transition-duration: 1200ms; transition-timing-function: ease-in;')
-    mainMenu.style.opacity = 1;
     document.getElementById('hrMain').style.width = '24rem';
-    setTimeout(() => {document.getElementById('sliders').style.opacity = 1}, 300)
+    setTimeout(() => {inputContainer.style.opacity = 1; mainMenu.style.opacity = 1;}, 300)
     setTimeout(() => {
-        document.getElementById('hrMain').style.width = '9.2rem';
+        document.getElementById('hrMain').style.width = '14.2rem';
     }, 400)
 }
